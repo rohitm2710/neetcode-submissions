8cc68ed5310230737d4848058fd30d43b1bd0433
Rohit Majumder <rohitmajumder05@gmail.com>
2026-08-19 10:21:24 +0530

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.cpp
@@ -0,0 +1,32 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 'c';
+}();
+class Solution {
+private:
+int curr=0;
+    void dfs(vector<vector<int>>& grid,int i,int j){
+        if(i<0||j<0||i==grid.size()||j==grid[0].size()||grid[i][j]==0) return;
+        grid[i][j]=0;
+        curr++;
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+    }
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int max_val=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    dfs(grid,i,j);
+                    max_val=max(max_val,curr);
+                    curr=0;
+                }
+            }
+        }
+        return max_val;
+    }
+};
